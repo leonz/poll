c13ef61546a7982662face4e08683a716bcd9f52
Leon Zaruvinsky <leon@lzaruvinsky.com>
2013-12-06 02:06:21 -0500

    Added requirements for the index page

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+$title = "Index Page";
+require("includes/header.php");
+
+echo "Hello World";
+
+
+/*
+
+
+1.  Select box - Radio buttons or checkbox?
+
+
+2. 3 Text boxes (choices)
+ - If text box is empty = choice is removed
+ - If text box is not empty = add as a choice to poll
+
+3. Link "Add more choices"
+ - When clicking this link, add another text box with another ID
+ - Max out at 10 (remove link when 10 text boxes appear)
+
+4. Submit button - create this poll
+
+*/
+
+?>
