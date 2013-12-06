e199f41238da537082a20c27fe56b234c72c4344
Leon Zaruvinsky <leon@lzaruvinsky.com>
2013-12-06 18:39:50 -0500

    First version of the Database class

---
+++ classes/database.php
@@ -0,0 +1,43 @@
+<?php
+/****************************************************
+ Database Class
+ Manages database connections and queries.
+ Version 0.0 - LZ - 12/6/2013
+ ***************************************************/
+
+class Database {
+
+	private $host, $username, $password, $database;
+	private $db;
+	
+	/** Opens a connection the database */
+	public function __construct() {
+		$this->host	= "";
+		$this->username	= "";
+		$this->password	= "";
+		$this->database	= "";
+
+		$this->db = mysqli_connect($this->host, $this->username, $this->$password, $this->database)
+		or die('Error: ' . mysqli_error($this->db));
+		
+		return true;
+	}
+	
+	/** Runs the specified query on the database
+	 *  Precondition: The query is already sanitized!
+	 */
+	public function query($query) {
+		$result = mysqli_query($this->db, $query);
+		if (!$result) die('Invalid query: ' . mysqli_error($this->db));
+		return $result;
+	}
+	
+	/** Closes the connection to the database */
+	public function __destruct() {
+		$mysqli->close($this->db)
+		OR die("There was a problem disconnecting from the database.");
+	}
+
+}
+
+?>
