f272601ce6558fc7ce3ca05f24b90fee33de6eff
Leon Zaruvinsky <leon@lzaruvinsky.com>
2013-12-16 22:14:02 -0500

    Created specifications for index page

--- index.php
+++ index.php
@@ -1,19 +1,66 @@
 <?php
 
-$title = "Create your own poll with Make My Poll!";
-require("includes/header.php");
+// USE HEREDOCS for each of the three scenarios
 
-if (isset($_GET['display']) && htmlentities($_GET['display']) == "results") {
+/********** FORM IS SUBMITTED */
+
+// check that at least two choice fields are filled
+// htmlentities() question and each choice
+// remove = from question and each choice
+
+// is the form successful?
+
+// mysqlescaperealstrings and submit the poll to the database
+// create a made-poll cookie
+// extract the new poll's id
+// forward to viewpoll.php?id=urlID
+
+
+// is the form NOT successful?
+
+// show errors that it wasn't successful
+// populate form with previous fields
+// show choice boxes if they are populated, hide if not
+// only fill the top choice boxes? (push all answers up)
+
+/******************8
+
+for each filled answer in array of choices_list, create a visible textbox and ++ variable
+hide 10 - variable textboxes and include link
+
+*/
+
+// is the form not even submitted?
+
+// check for a cookie that a poll wasn't recently made
+// check for the display=error to create an error banner
+// display the poll form with 3 textboxes, 7 hidden 
+
+
+
+$title = 'Create your own poll with Make My Poll!';
+require('includes/header.php');
+
+if (isset($_GET['display']) && htmlentities($_GET['display']) == 'results') {
 	echo '<div id="error">There was an error trying to access that page.</div>';
 }
 
+
+echo '<h1>Make My Poll</h1>';
+
+if (isset($_COOKIE['made-poll'])) {
+	echo '<p>We\'ve detected that you just made a poll.  Please wait a little bit before you make another one.</p>';
+
+} else {
+
 ?>
+
+<p>Create your own poll and share it with your friends.  Get started now - no registration needed!</p>
 
 <form name="create" method="post" action="create.php">
 
 Question: 
 <input type="text" name="question" id="question" /><br />
-
 
 Choice 0:
 <input type="text" name="choice_list[]" class="choice" value="" /><br />
@@ -36,17 +83,22 @@ Choice 8:
 Choice 9:
 <input type="text" name="choice_list[]" class="choice" value="" /><br />
 
-<input type="submit" name="submit" value="Create my poll" />
+<input type="checkbox" name="type" value"checkbox" /> Allow people to vote for multiple choices?<br />
+
+
+<input type="submit" name="submit" value="Make My Poll!" />
 
 </form>
+
 <?php
 
+}
 
+?>
+<?php
 /*
 
-
 1.  Select box - Radio buttons or checkbox?
-
 
 2. 3 Text boxes (choices)
  - If text box is empty = choice is removed
