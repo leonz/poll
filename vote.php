06f02a4a282ea7b16cab3d08650e7244f7f9f70d
Leon Zaruvinsky <leon@lzaruvinsky.com>
2013-12-26 21:57:23 -0500

    Just some more cleanup

--- vote.php
+++ vote.php
@@ -1,18 +1,6 @@
 <?php
 /** Processes the vote casting for a poll */
 
-/********
-
-Goals for this page:
-
- - Extract the ID from the URL and all of the posted variables (voting results)
- - If the required variables aren't set OR an 'already voted' cookie is set - Redirect to viewpoll
- - Submit a query to the database to ++ the vote values for each voted on choice 
- - Set a cookie that the user has already voted on this poll
- - Redirect to viewpoll.php?id=pollid&display=results
-
-********/
-
 if (!isset($_GET['id'])) {
 	header('Location: index.php?display=error');
 	exit;
